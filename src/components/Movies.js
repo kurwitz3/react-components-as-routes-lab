e7b222d4689a8381425391b28375ad5f342d9c9f
kurwitz3 <kurwitz3@aol.com>
2021-03-06 18:57:01 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,19 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie =>{
+          return <div>
+            Name: {movie.title}<br></br>
+            <br></br>
+            Time:{movie.time}
+            <ul>
+              {movie.genres.map(g =>{
+                return <li>{g}</li>
+              })}
+            </ul>
+            </div>
+        })}
     </div>
   );
 };
